80a9467f9fcf4275c0f834c35c4178b05ed1e43a
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-04-20 01:22:03 +0200

    Initalize finished events

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// import './css/normalize.css';
-// import './css/style.css';
+import './css/normalize.css';
+import './css/style.css';
+import Event from './Event';
+
+const e = new Event();
+
+e.init();
